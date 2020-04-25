bf1cf863256bb1ffcbd691b6bb81db7f8ef815c6
Yasukim <yasuhisakimura91@gmail.com>
2020-04-25 12:18:55 +0900

    eval_picture.py commit

---
+++ eval_picture.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 25 11:20:49 2020
+
+@author: kimurayasuhisa
+"""
+
+#綾鷹を選ばせるプログラム
+
+from keras import models
+from keras.models import model_from_json
+from keras.preprocessing import image
+import numpy as np
+
+model = model_from_json(open('model_data/test_predict.json').read())
+model.load_weights('model_data/test_predict.hdf5')
+print('model loaded')
+#%%
+
+categories = ["PS4","ぬいぐるみ","イス"]
+#%%
+#画像を読み込む
+img_path = str(input())
+
+img = image.load_img(img_path,target_size=(150, 150, 3))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+
+#予測
+features = model.predict(x)
+
+#予測結果によって処理を分ける
+if features[0,0] == 1:
+    print ("選ばれたのはPS4でした")
+
+elif features[0,1] == 1:
+    print ("選ばれたのは、ぬいぐるみでした")
+    
+elif features[0,2] == 1:
+    print ("選ばれたのは、イスでした")
+
+else:
+    print("Error")
